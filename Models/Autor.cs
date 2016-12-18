90426500b49c88da99a1b576df2e6346af5d0026
Omega017 <joseeh.8@gmail.com>
2016-12-18 18:03:30 -0300

    Modified: Added TipoAutor enum and attribute

--- Models/Autor.cs
+++ Models/Autor.cs
@@ -4,10 +4,14 @@ namespace Publicaciones.Models
     {
         public string AutorId { get; set; }
 
+        public TipoAutor TipoAutor { get; set; }
+
         public string Rut { get; set; }
         public virtual Persona Persona { get; set; }
 
         public string PublicacionId { get; set; }
         public virtual Publicacion Publicacion { get; set; }
     }
+
+    public enum TipoAutor { Principal, Correspondiente, Normal };
 }
