feb4f03bcfc9eef59619d7fc5b37ec502db0a5ff
Omega017 <joseeh.8@gmail.com>
2016-12-18 18:05:46 -0300

    Added: Revista class

---
+++ Models/Revista.cs
@@ -0,0 +1,11 @@
+namespace Publicaciones.Models
+{
+     public class Revista
+     {
+         public string RevistaId { get; set; }
+
+         public string Nombre { get; set; }
+
+         public string Issn { get; set; }
+     }
+}
