d9e06b7a4b18820e9017ad83d0596b00cb6a85c9
Omega017 <joseeh.8@gmail.com>
2016-12-18 18:07:02 -0300

    Moddifies: added Revista navigation

--- Models/Publicacion.cs
+++ Models/Publicacion.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
+using System;
 namespace Publicaciones.Models
 {
     public class Publicacion
     {
         public string PublicacionId { get; set; }
 
+        public DateTime FechaRevista { get; set; }
+        
+        public string RevistaId { get; set; }
+        public virtual Revista Revista { get; set; }
+
+        public DateTime FechaWeb { get; set; }
+
         public int PagInicio { get; set; }
+
+        public virtual List < Paper > Papers { get; set; }
+
     }
 }
