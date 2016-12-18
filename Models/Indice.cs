fdb462ff5338c1626f1786f5506c58d1e36eaf01
Omega017 <joseeh.8@gmail.com>
2016-12-18 18:05:07 -0300

    Added: Indice class

---
+++ Models/Indice.cs
@@ -0,0 +1,10 @@
+namespace Publicaciones.Models
+{
+     public class Indice
+     {
+         public string IndiceId { get; set; }
+
+         public string Nombre { get; set; }
+     }
+}
+        
